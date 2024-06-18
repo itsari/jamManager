67b5cef655a077e0a55c8914922d10f7814f8d7f
itsari <itsari@live.com>
2024-06-18 14:37:16 +0300

    working on chords location

--- helper/parseLyrics.js
+++ helper/parseLyrics.js
@@ -60,16 +60,23 @@ function parseLyrics(text) {
   const lines = text.split("\n");
   const parsedLines = lines.map((line) => {
     const chords = [];
-    const lyrics = line.replace(
+    let lyricsLine = line.replace(
       /\[([A-G][#b]?(?:m|maj|min|dim|aug|sus|add)?\d*)\]/g,
-      (match, chord) => {
-        chords.push(chord);
+      (match, chord, offset) => {
+        chords.push({ chord, offset });
         return " ".repeat(chord.length);
       }
     );
-    return { chords, lyrics };
+    // Remove trailing spaces from lyrics line
+    lyricsLine = lyricsLine.trimRight();
+    return { chords, lyrics: lyricsLine };
   });
   return parsedLines;
 }
 
 module.exports = parseLyrics;
+
+
+
+
+
